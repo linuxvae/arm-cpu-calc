260c730ab696a4097125fce82107c32d33b11a50
贺利祥 <he-lixinag@szreach.com>
2018-01-16 18:59:04 +0800

    修改问题

--- cputest.sh
+++ cputest.sh
@@ -1,30 +1,34 @@
 #!/bin/sh
 echo '$1 threads cpu'
 
+i=0
 for tid in `ls /proc/$1/task/`
 do
-	tm=`cat  /proc/$1/task/$tid/stat |awk  -F " " '{print $14+$15}'`
-	echo $tm
+	tm1[$i]=`cat  /proc/$1/task/$tid/stat |awk  -F " " '{print $14+$15}'`
+	let i++
 done
-
 total=`cat /proc/stat |awk -F " " 'NR==1{print  $2+$3+$4+$5+$6+$7+$8}'`
 echo $total
 
 sleep 10s
 
+i=0
 for tid in `ls /proc/$1/task/`
 do
-        tm2=`cat  /proc/$1/task/$tid/stat |awk  -F " " '{print $14+$15}'`
-        echo $tm2
+        tm2[$i]=`cat  /proc/$1/task/$tid/stat |awk  -F " " '{print $14+$15}'`
+        let i++
 done
 
 total2=`cat /proc/stat |awk -F " " 'NR==1{print  $2+$3+$4+$5+$6+$7+$8}'`
 echo $total2
 
+i=0
 total3=`expr $total2 - $total`
-tm3=`expr $tm2 - $tm`
-cpus=`cat /proc/cpuinfo| grep "processor"| wc -l`
-echo $cpus $total3 $tm3
-
-tcpu=`expr $tm3 \* $cpus \* 100 / $total3`
-echo "thread cps: $tcpu"
+for tid in `ls /proc/$1/task/`
+do
+	tm3=`expr ${tm2[$i]} - ${tm1[$i]}`
+	cpus=`cat /proc/cpuinfo| grep "processor"| wc -l`
+	tcpu=`expr $tm3 \* $cpus \* 100 / $total3`
+	echo "thread $tid cps: $tcpu"
+	let i++
+done
